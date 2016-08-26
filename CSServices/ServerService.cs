76161bb03d9ce068cc693138358e8ad11ad44f46
J C <jarak_jakar@hotmail.com>
2016-08-26 14:46:23 +1200

    Continued working on the hashing algorithms.

--- CSServices/ServerService.cs
+++ CSServices/ServerService.cs
@@ -67,20 +67,49 @@ namespace CSServices
 
         public static IEnumerable<segmentDetails> breakFileIntoSegments(string fullfilepath)
         {
-            const int windowWidth = 64; // Size of the window in bytes
+            const int windowWidth = 32; // Size of the window in bytes
             const int primeNumber = 3511;  // Base prime number used in the RK calculations
             const long segmentDelimiter = (1 << 11) - 1; // Should mean that I get 2kiB segments, on average
+            long rollingHash = 0; // The variable to store the value of the rolling hash
+            int bufferSize = windowWidth * 1024;  // Do a reasonably large number of bytes in each read from the filestream
+            byte[] streamBuffer = new byte[bufferSize];
+            int bufferPos = 0;
+            int lastStartPos = 0;
+            var strongHasher = System.Security.Cryptography.SHA256.Create();
 
             long primePower = primeNumber;
             for(int i = 0; i < windowWidth; i++)
             {
-                primePower *= primeNumber; // Create the full power of the prime
+                primePower *= primeNumber; // Create the full power of the prime - this is slow, but only done once per method invocation
             }
-
-
 
             using (FileStream fs = new FileStream(fullfilepath, FileMode.Open, FileAccess.Read))
             {
+                int bytesRead = fs.Read(streamBuffer, 0, bufferSize);
+
+                rollingHash = startHash(streamBuffer, windowWidth, primeNumber);
+
+                bufferPos = windowWidth;
+
+                while(bytesRead > 0)
+                {
+                    /*rollingHash -= (streamBuffer[bufferPos - windowWidth] * primePower);
+                    rollingHash *= primeNumber;
+                    rollingHash += streamBuffer[bufferPos];*/
+                    for (int i = 0; i < length; i++)
+                    {
+                        rollingHash = ((rollingHash - (streamBuffer[bufferPos - windowWidth] * primePower)) * primeNumber) + streamBuffer[bufferPos]; // Move the rolling hash by one
+                        if ((rollingHash & segmentDelimiter) == 0)
+                        {
+                            yield return new segmentDetails(bufferPos, bufferPos - lastStartPos, strongHasher.ComputeHash(streamBuffer, bufferPos, (bufferPos - lastStartPos)));
+                        }
+
+                        lastStartPos = bufferPos;
+                        bufferPos++; 
+                    }
+
+                    bufferPos = 0;
+                }
 
                 byte[] moose = new byte[12];
                 yield return new segmentDetails(0, 58, moose);
@@ -102,6 +131,19 @@ namespace CSServices
                 this.segmentLength = segmentLength;
                 this.hashValue = hashValue;
             }
+        }
+
+        private static long startHash(byte[] firstBit, int hashlength, int primeNumber)
+        {
+            long returnHash = 0;
+
+            for (int i = 0; i < hashlength; i++)
+            {
+                returnHash *= primeNumber;
+                returnHash += firstBit[i];
+            }
+
+            return returnHash;
         }
     }
 
